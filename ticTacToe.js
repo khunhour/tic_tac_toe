3e796a26642930544cf7977bff7a044f2d365659
Khunhour Morm <khonhourmorm@gmail.com>
2021-09-25 08:15:59 +0900

    finish unbeatable ai and added depth and clean up code add comments

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,19 +1,19 @@
 const module = (function(){
-    let player1Scores=[];
-    let player2Scores=[];
     let winner="";
-    let player1 = true;         //check player1 turn or not
-    let tileArray = [1, 2, 3, 4, 5, 6, 7,8 ,9];
+    let currentPlayer = "human";         //human moves first
+    let currentBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    let aiMark = "O";
+    let humanMark ="X";
 
-    const winningCases = {
-        case1: [1,2,3],
-        case2: [4,5,6],
-        case3: [7,8,9],
+    const winningCases = {              //all winning combos
+        case1: [0,1,2],
+        case2: [3,4,5],
+        case3: [6,7,8],
         case4: [1,4,7],
         case5: [2,5,8],
-        case6: [3,6,9],
-        case7: [1,5,9],
-        case8: [3,5,7],
+        case6: [0,3,6],
+        case7: [0,4,8],
+        case8: [2,4,6],
     }
 
     const tiles = document.querySelectorAll('.tile');
@@ -33,12 +33,13 @@ const module = (function(){
             button.addEventListener("click", restartGame);
         });
     };
+
     const removeEvent = () =>{
         tiles.forEach((tile) => {
             tile.removeEventListener('click', markPlayerTile);
-            tile.classList.remove("redM")
         });
     };
+
     const removeMarkAndClass = () =>{
         tiles.forEach((tile) => {
             tile.textContent = "";
@@ -48,110 +49,152 @@ const module = (function(){
     
     const markPlayerTile = function(e){
         let tileNum = Number(e.target.id);
-        console.log("x's tile Num:" + tileNum);
         const tile = document.getElementById(`${tileNum}`);
-
-        if(player1 === true){                   //marking player1 move
-            removeArrayElement(tileNum);
-            console.log("current Array : " + tileArray);
-
-            tile.textContent="X";
+        if(currentPlayer === "human"){                                   //marking player1 move
+            tile.textContent = humanMark;
             tile.classList.add("redMark");
-            player1Scores.push(tileNum);        //push index position to player1Scores to store as an array
-            winner = checkWin(player1Scores);
-
             tile.removeEventListener('click', markPlayerTile);
-            displayResult(winner);
-
-            player1 = !player1;
             
-            markComputerTile();  //passing turn to computer
-            
+            currentBoard[tileNum] = humanMark;                  //update current board      
+            winner = checkWin(currentBoard, humanMark);         //check to see if there is a winner
+            declareWinner(winner);                              
+            currentPlayer = "computer";                         //player2's turn now
+            markComputerTile();                                 //passing turn to computer
         }
     };
+
     const markComputerTile = () =>{
-        tileNum = pickRandomTile(tileArray);
-        if(tileNum === undefined){              //incase at the end arraylength =0 cannot find random number means game end so no need
+        let bestPlayInfo = minimax(currentBoard, 0 ,aiMark);       //get the best move for the Ai mark(bestPlayerInfo={index:?, score:?})
+        tileNum = bestPlayInfo.index;
+        const tile = document.getElementById(`${tileNum}`);
+        
+        if(tileNum === undefined){                              //incase at the end, arraylength =0 cannot find valid move(to avoid errors)
             return;
         }
-        console.log("o's tile Num:" + tileNum);
-        const tile = document.getElementById(`${tileNum}`);
+        if(currentPlayer === "computer"){
+            tile.textContent= aiMark;
+            tile.classList.add("blueMark");
+            tile.removeEventListener('click', markPlayerTile);
 
-        removeArrayElement(tileNum);
-        console.log("current Array : " + tileArray);
-        tile.textContent="O";
-        tile.classList.add("blueMark");
-        player2Scores.push(tileNum);        ////push index position to player2Scores to store as an array
-        winner = checkWin(player2Scores);
-        
-        tile.removeEventListener('click', markPlayerTile);
-        displayResult(winner);
-        player1 = !player1;
+            currentBoard[tileNum] = aiMark;
+            winner = checkWin(currentBoard, aiMark);
+            declareWinner(winner);
+            currentPlayer = "human";
+        }
     };
 
-
-    const displayResult = (winner) => {
-        if(winner === true && player1 === true){
+    const declareWinner = (winner) => {
+        if(winner === true && currentPlayer === "human"){
             result.style.cssText = "display:flex;";
             removeEvent();
             announceText.textContent = "The Winner is X";
         }
-        else if(winner === true && player1 === false){
+        else if(winner === true && currentPlayer === "computer"){
             result.style.cssText = "display:flex;";
             removeEvent();
             announceText.textContent = "The Winner is O";
         }
-        else if(winner === false && player1Scores.length === 5 && player2Scores.length ===4){ 
+        else if(winner === false && findRemainingTile(currentBoard).length === 0){ 
             result.style.cssText = "display:flex;";
             announceText.textContent = "It's a Draw";
         }
     }
 
-    const checkWin = (player) => {
-        for(let i=1 ; i < 9; i++){             //loop over the 8 cases for winning
-
-            let intersectArray = player.filter(function(e) {        // intersect each cases with playerScores array
-                return winningCases[`case${i}`].indexOf(e) > -1;
-            });
-            if(intersectArray.sort().toString() === winningCases[`case${i}`].toString()){       //if after intersection, the case = intersection then i is the winning case
-
-                console.log("someone won!");
-                return true;                // true means someone has one
+    const checkWin = (board, playerMark) => {
+        let playerPosition=[];                                              //find the positions/index that player 1 and player 2 has marked and put them into array for comparing
+        for(let i=0; i<board.length; i++){
+            if(board[i] === playerMark){
+                playerPosition.push(i);
             }
         }
-        return false;          //false means someone has not wont or it is a draw
+        for(let i=1 ; i < 9; i++){                                          //loop over the 8 cases for winning
+
+            let intersectArray = playerPosition.filter(function(e) {        // intersect each cases with playerPosition array
+                return winningCases[`case${i}`].indexOf(e) > -1;            //return the matched element within each case
+            });
+            if(intersectArray.sort().toString() === winningCases[`case${i}`].toString()){       //if after intersection, the case = intersected Array then i is the winning case
+                
+                return true;                                                // true means someone has one
+            }
+        }
+        return false;                                                       //false means someone has not won or it is a draw
     }
 
     const restartGame = (e) =>{
         result.style.cssText = "display:none;";
         removeMarkAndClass();
         tileEventListener();
-
-        player1Scores=[];
-        player2Scores=[];
-        tileArray=[1,2,3,4,5,6,7,8,9];
-        player1 = true;
+        currentBoard = [0,1,2,3,4,5,6,7,8];
+        currentPlayer = "human";
         winner ="";
     }
-    const removeArrayElement = (value) =>{
-        let index = tileArray.indexOf(value);   //find index of value
-        tileArray.splice(index, 1);             //remove that value from array
+
+    const findRemainingTile = (board) => {             
+        let remainingTiles = board.filter(tile => tile != "X" && tile != "O");      //return an array that does not contain X and O
+        return remainingTiles;
     }
-    const pickRandomTile = (tileArray) =>{
-        let randomIndex = Math.floor(Math.random()*(tileArray.length-1));
-        console.log("length: " +tileArray.length);
-        console.log("choose index: "+randomIndex);
-        
-        console.log(tileArray[randomIndex]);
-        return tileArray[randomIndex];
+
+    // const removeArrayElement = (value) =>{
+    //     let index = currentBoard.indexOf(value);   //find index of value
+    //     currentBoard.splice(index, 1);             //remove that value from array
+    // }
+
+    // const pickRandomTile = (array) =>{
+    //     let randomIndex = Math.floor(Math.random()*(array.length));
+    //     return array[randomIndex];
+    // }
+
+    function minimax(board, depth, currentMark) {           //adding depth because trying the fastest way to win.
+        let availableTile = findRemainingTile(board);
+
+        if(checkWin(board, humanMark)){                     
+            return {score: depth-10 };
+        }
+        else if(checkWin(board, aiMark)){
+            return {score: 10-depth };                             
+        }
+        else if(availableTile.length === 0){
+            return {score: 0};
+        }
+
+        let moves = [];                                     //for recording all the possible moves id and scores
+        for (let i=0; i<availableTile.length; i++){
+            let move ={};                                   //individual move
+            move.index = board[availableTile[i]];
+            board[availableTile[i]] = currentMark;          //test the that move by placing the current Mark on the board
+            if(currentMark === aiMark){
+                var result = minimax(board, depth+1, humanMark);     //run through the minimax function again to see how it scores for human(human is minimizing)
+                move.score = result.score;
+            }
+            else{
+                var result = minimax(board, depth+1, aiMark);
+                move.score = result.score;
+            }
+            board[availableTile[i]] = move.index;            //revert board back to normal for testing other moves
+            moves.push(move);                                //moves should look like this : moves=[{index:3, score: 10},{index:4, score -10}] 
+        }
+        let bestMove=null;                                   //finding the best move out of those recorded moves based on scores
+
+        if(currentMark === aiMark){                          //for the AI we want the highest out of all the moves
+            let bestScore = -Infinity;                       //set as a lowest number can be -10000 also
+            for(let i=0; i < moves.length; i++){             
+                if(moves[i].score > bestScore){
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }else{                                               //for human player (minimizing), we want the lowest scores out of all the moves
+            let bestScore = Infinity;   
+            for(let i=0; i < moves.length; i++){
+                if(moves[i].score < bestScore){
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+        return moves[bestMove];                               // return the best move {index:? score:?}
     }
-    return{initiateGame, winningCases, tileArray, checkWin,player1Scores, player2Scores,winner
-    };
+    return{initiateGame};
 })();
 
 module.initiateGame();
-
-const playerScores = (playerNum, position) =>{
-
-
-};
